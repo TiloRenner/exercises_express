119ab26fb1dc23cf079a1c4564850e64dd3c04f1
DESKTOP-AAHU0IQ\Tilo <info@tilorenner.com>
2023-11-16 16:09:47 +0100

    step5

--- index.js
+++ index.js
@@ -4,32 +4,43 @@ import { fileURLToPath } from 'url'
 import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
+const PORT = 3000;
 
 var app = express();
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static('public'))
 
+app.set('views', './views')
+app.set('view engine','ejs')
+
+app.get('/test-ejs', (req,res) => {
+    res.render('index' ,{myTitle: "my first Title"})
+})
+
+
+app.get('/test-ejs2', (req,res) => {
+    res.render('index2' ,{users : ['Bob', 'John', 'Jane' ]})
+})
 
 app.get('/',(req,res) => {
     res.send("Hallo BenutzerX");
-
-
 });
 
-
 app.post('/', (req,res) => {
-
     res.sendFile(path.join(__dirname,'hello.html'));
 });
 
-
 app.delete('/',(req,res) => {
-
     res.status(200).json({"good" : "yep"});
 });
 
-app.listen(3000,() => {
+/*app.listen(PORT,() => {
     console.log('Hello');
+})*/
 
-})
+app.listen(
+    PORT,()=>console.log(`http://localhost:${PORT}`)
+);
 
 

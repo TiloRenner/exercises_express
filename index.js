3bb137d92560e00858d9c966b9e7b7316472695f
DESKTOP-AAHU0IQ\Tilo <info@tilorenner.com>
2023-11-16 15:07:29 +0100

    basic routing

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 import express from "express";
+import path from 'path';
+import { fileURLToPath } from 'url'
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 
 var app = express();
 
 
 app.get('/',(req,res) => {
-    res.send("Hallo Benutzer");
+    res.send("Hallo BenutzerX");
 
 
-} )
+});
+
+
+app.post('/', (req,res) => {
+
+    res.sendFile(path.join(__dirname,'hello.html'));
+});
+
+
+app.delete('/',(req,res) => {
+
+    res.status(200).json({"good" : "yep"});
+});
 
 app.listen(3000,() => {
     console.log('Hello');
 
 })
+
+

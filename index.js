b1052231107dec5964d7667806dff0addde744e1
DESKTOP-AAHU0IQ\Tilo <info@tilorenner.com>
2023-11-17 17:48:54 +0100

    commit 1711

--- index.js
+++ index.js
@@ -12,15 +12,13 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
 //app.use(methodOverride('X-HTTP-Method-Override'))
-app.use(methodOverride('_method'))
-
+app.use(methodOverride('_methodx'))
 app.set('views', './views')
 app.set('view engine','ejs')
 
 app.get('/test-ejs', (req,res) => {
     res.render('index' ,{myTitle: "my first Title"})
 })
-
 
 app.get('/test-ejs2', (req,res) => {
     res.render('index2' ,{users : ['Bob', 'John', 'Jane' ]})
@@ -34,8 +32,24 @@ app.get('/',(req,res) => {
     res.send("Hallo BenutzerX");
 });
 
+app.post('/showPost',(req,res) => {
+    res.send(req.body.firstName);
+    console.log("PostNameResult:", req.body);
+});
+
+app.get('/showGet',(req,res) => {
+
+    res.send([req.query.firstName , req.query.lastName]);
+    console.log("GetNameResult:" ,req.query);
+});
+
+app.get('/number/:id',(req,res) => {
+    res.send(`The number is ${req.params.id}`);
+    console.log("Ex9 - Params")
+});
+
 app.post('/', (req,res) => {
-    res.sendFile(path.join(__dirname,'hello.html'));
+    res.sendFile(path.join(__dirname,'hello2.html'));
 });
 
 app.put('/', (req,res) => {
